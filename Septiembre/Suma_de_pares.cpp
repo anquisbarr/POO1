0b34c83dd29d6567050caa2c21558278e0b68148
anquisbarr <39910724+anquisbarr@users.noreply.github.com>
2018-09-13 10:18:11 -0500

    Create Suma_de_pares.cpp

---
+++ Septiembre/Suma_de_pares.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int sumapar=0,numero;
+    cout<<"Digite numero: " <<endl;
+    cin>>numero;
+    for(int i=0; i<=numero;i+=2){
+        sumapar=sumapar+i;
+        }
+    cout << sumapar;
+    }
