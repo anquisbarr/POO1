c3be8e8771206d9f78bd330f5b7743d7ab1289fa
anquisbarr <39910724+anquisbarr@users.noreply.github.com>
2018-10-01 18:44:27 -0500

    Create Ejemplo_simple_de_Switch.cpp

---
+++ Octubre/Ejemplo_simple_de_Switch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int numero;
+    cout<<"Digite un numero entre el 1-5"<<endl;
+    cin >> numero;
+    
+    switch(numero){
+        case 1: cout <<"Es el numero 1"; break;
+        case 2: cout <<"Es el numero 2"; break;
+        case 3: cout <<"Es el numero 3";break;
+        case 4: cout <<"Es el numero 4";break;
+        case 5: cout <<"Es el numero 5";break;
+        default: cout<<"No esta en el rango de 1-5";break;
+    }
+    return 0;
+}
